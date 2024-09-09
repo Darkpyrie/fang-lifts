77147631d3a86113a5fc5cedeb543a8db7c7cfb0
Solareon <769465+solareon@users.noreply.github.com>
2024-09-09 12:46:33 +0200

    fix(client): add dependency check

--- client/client.lua
+++ client/client.lua
@@ -1,3 +1,5 @@
+if not lib.checkDependency('qbx_core', '1.15.0') then lib.print.error('qbx_core v1.15.0 is required for dark-lifts') return end
+
 local function isDisabled(name, level) --function to check if a floor/elevator is job locked in config.lua
     if name.JobLocked then --checks if elevator is job locked
         return not exports.qbx_core:HasGroup(name.JobAccess)
